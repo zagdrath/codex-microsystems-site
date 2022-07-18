c282851d47efbf2ee1f2027e6374c25038fef3a9
Cody Wellman <cody@codexmicro.systems>
2022-07-18 09:47:47 -0400

    Fixed logo

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,9 @@ export default function Header() {
     <div className="header">
       <div className="nav">
         <Link href='/' className='nav-logo'>
-          <Image src='/images/logo.png' alt='Codex Microsystems' height={40} width={110} className='nav-logo-image' />
+          <a>
+            <Image src='/images/logo.png' alt='Codex Microsystems' height={40} width={110} className='nav-logo-image' />
+          </a>
         </Link>
         <ul className='nav-items'>
           <li className='nav-item'>
