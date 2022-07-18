803ba59de67fc6d588c106dc4141c5471e9b6028
Cody Wellman <cody@codexmicro.systems>
2022-07-17 23:20:51 -0400

    [Header] [7/17/2022] Add non-responsive branch

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,14 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import { useState } from 'react'
 
 export default function Header() {
-  const [isNavExpanded, setIsNavExpanded] = useState(false)
-
   return (
     <div className="header">
       <div className="nav">
         <Link href='/' className='nav-logo'>
           <Image src='/images/logo.png' alt='Codex Microsystems' height={40} width={110} className='nav-logo-image' />
         </Link>
-        <ul className={isNavExpanded ? "nav-items-expanded" : "nav-items"}>
+        <ul className='nav-items'>
           <li className='nav-item'>
             <Link href='/'>
               <a className='nav-item-link'>Home</a>
@@ -33,9 +30,6 @@ export default function Header() {
             </Link>
           </li>
         </ul>
-        <button onClick={() => { setIsNavExpanded(!isNavExpanded) }} className='menu'>
-          <Image src='/images/menu.svg' alt='Menu' height={30} width={30} />
-        </button>
       </div>
     </div>
   )
